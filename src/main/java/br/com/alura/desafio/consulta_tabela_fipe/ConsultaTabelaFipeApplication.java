20332120e9cd08e54f3a42b0e5bd99d946960024
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2025-03-10 14:02:09 -0300

    first commit

---
+++ src/main/java/br/com/alura/desafio/consulta_tabela_fipe/ConsultaTabelaFipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafio.consulta_tabela_fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaTabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaTabelaFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/desafio/consulta_tabela_fipe/ConsultaTabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafio.consulta_tabela_fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaTabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
